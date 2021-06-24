4704ad1e62463da384e1070d4a54fe8128f77fdd
Zianh-Yang <zxc7889@gmail.com>
2021-06-24 14:10:16 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './App.css';
+import MainPageComponent from "./main/index.js"
+function App() {
+  return (<MainPageComponent/>
+  );
+}
+export default App;
---
+++ src/main/index.js
@@ -0,0 +1,64 @@
+import './index.css';
+import axios from "axios";
+import React from 'react';
+function MainPage(){
+            const [products, setProducts]= React.useState([]);
+        React.useEffect(
+            function() {
+                axios
+                .get("https://a66c3f81-3920-4061-b351-053884664dfa.mock.pstmn.io/products")
+                .then(function(result){
+                    const products = result.data.products;
+                    setProducts(products);
+                }).catch(function(error){
+                   console.error('에러 발생 :',error); 
+                });
+            },[])    
+        
+
+    return (
+            <div>
+        <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+            {
+                products.map(function(product, index){
+                        return( 
+                        <div className="product-card">
+                        <div>
+                         <img className="product-img" 
+                         src={product.imageUrl}/>
+                        </div>
+                        <div className="product-contents">
+                            <span className="product-name">
+                            {product.name}
+                            </span>
+                            <span className="product-price">
+                            {product.price}원
+                            </span>
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/icons/avatar.png"/>
+                                <span>{product.seller}</span>
+                            </div>
+                        </div>
+                    </div>
+                            );
+                    })
+                }
+            
+        </div>
+      </div>
+      <div id="footer"></div>
+      </div>
+    );
+}
+
+export default MainPage;
